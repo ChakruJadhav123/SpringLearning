5b07a5cfc6caa73781436bfafd005740907f90a3
ChakruJadhav123 <chakrujadhav5456@gmail.com>
2025-09-15 01:27:33 +0530

    Second Commit Spring Adding Dependancies

---
+++ SpringCration/src/main/java/com/TightCoupling/UserManager.java
@@ -0,0 +1,8 @@
+package com.TightCoupling;
+
+public class UserManager {
+   private  UserDatabase userDatabase=new UserDatabase();
+    public String getUserInfo(){
+        return userDatabase.getUserDetails();
+    }
+}
---
+++ SpringCration/src/main/java/com/loose/coupling/UserDataProvider.java
@@ -0,0 +1,5 @@
+package com.loose.coupling;
+
+ interface UserDataProvider {
+    String getUserDetails();
+}
