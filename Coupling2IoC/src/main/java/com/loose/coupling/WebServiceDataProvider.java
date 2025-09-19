3f938d25f392c3d94d205f32bdbf13b6ee669129
ChakruJadhav123 <123489403+ChakruJadhav123@users.noreply.github.com>
2025-09-19 23:44:34 +0530

    Update WebServiceDataProvider.java

--- Coupling2IoC/src/main/java/com/loose/coupling/WebServiceDataProvider.java
+++ Coupling2IoC/src/main/java/com/loose/coupling/WebServiceDataProvider.java
@@ -1,7 +1,5 @@
 package com.loose.coupling;
-
 public class WebServiceDataProvider implements UserDataProvider  {
-
     @Override
     public String getUserDetails() {
         return "Fetching Details From Web Service";
